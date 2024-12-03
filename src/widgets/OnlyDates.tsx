b8eff8347412f1643f163c6d89990b3f465aa6e8
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-12-04 01:34:52 +0400

    Refactor date selection logic and UI components

--- src/widgets/OnlyDates.tsx
+++ src/widgets/OnlyDates.tsx
@@ -1,88 +1,60 @@
 import Circle from '../components/Circle'
 import Container from '../components/Container'
 import { periods } from '../mock-data'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination, EffectFade } from 'swiper/modules'
-import classNames from 'classnames'
 import { PeriodDate } from '../mock-data/types'
-import { useCallback, useRef, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import useMediaQuery from '../hooks/useMediaQuery'
 
-import styles from './classes.module.scss'
-import 'swiper/css'
-import 'swiper/swiper-bundle.css'
-import 'swiper/css/pagination'
-import 'swiper/css/effect-fade'
+import DateSwiper from '../components/Swiper'
+import PeriodSwitcher from '../components/PeriodSwitcher'
+import Title from '../components/Title'
 
 export default function OnlyDates() {
-  const [periodIndex, setPeriodIndex] = useState(0)
-  const [dates, setDates] = useState<PeriodDate[]>(periods[periodIndex].dates)
-
   const isMobile = useMediaQuery()
 
+  const [periodIndex, setPeriodIndex] = useState<number>(0)
   const [activeIndex, setAcitveIndex] = useState(0)
+  const [previousActiveIndex, setPreviousActiveIndex] = useState<number | null>(
+    null,
+  )
 
-  const setIndex = useCallback((index: number) => {
+  const dates = useMemo(() => periods[periodIndex].dates, [periodIndex])
+
+  const switchToNextPeriod = useCallback(() => {
+    if (!periods[periodIndex + 1]) setPeriodIndex(0)
+    setPeriodIndex((state) => state + 1)
+  }, [periodIndex])
+
+  const switchToPreviosPeriod = useCallback(() => {
+    if (!periods[periodIndex - 1]) setPeriodIndex(periods.length)
+    setPeriodIndex((state) => state - 1)
+  }, [periodIndex])
+
+  const setActivePeriodIndex = useCallback((index: number) => {
     setAcitveIndex(index)
   }, [])
+
+  const setPreviousPeriodIndex = useCallback((index: number) => {
+    setPreviousActiveIndex(index)
+  }, [])
+
   return (
     <Container>
-      <Circle currentPeriodIndex={2} numberOfCircles={5} />
-      <div
-        style={{
-          position: 'relative',
-          paddingLeft: 80,
-          paddingRight: 80,
-        }}
-      >
-        <img
-          className={classNames(
-            'previous-button',
-            styles.navigationButton,
-            styles.previousButton,
-          )}
-          src='/assets/icons/previous.svg'
-          alt='previous-button'
-        />
-        <img
-          className={classNames(
-            'next-button',
-            styles.navigationButton,
-            styles.nextButton,
-          )}
-          src='/assets/icons/next.svg'
-          alt='next-button'
-        />
-
-        <Swiper
-          modules={[Navigation, Pagination]}
-          navigation={{
-            disabledClass: styles.navigationHidden,
-            nextEl: '.next-button',
-            prevEl: '.previous-button',
-          }}
-          // pagination
-          slidesPerView='auto'
-          spaceBetween={80}
-          centeredSlides={false}
-        >
-          {dates.map((v, i) => (
-            <SwiperSlide
-              key={`${v.year}-${i}`}
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                cursor: 'pointer',
-                maxWidth: 320,
-                gap: 15,
-              }}
-            >
-              <p className={styles.year}>{v.year}</p>
-              <p className={styles.description}>{v.eventDescription}</p>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <Title />
+      <Circle
+        periods={periods}
+        setActivePeriodIndex={setActivePeriodIndex}
+        setPreviousPeriodIndex={setPreviousPeriodIndex}
+        previosPeriodIndex={previousActiveIndex}
+        currentPeriodIndex={activeIndex}
+      />
+      <PeriodSwitcher
+        switchToNextPeriod={switchToNextPeriod}
+        switchToPreviosPeriod={switchToPreviosPeriod}
+        periodsAmount={periods.length}
+        currentPeriod={periodIndex + 1}
+      />
+      <DateSwiper dates={dates} />
     </Container>
   )
 }
