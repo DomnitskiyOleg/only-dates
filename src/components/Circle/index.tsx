b374cc0d95f516c0286de29420e646eedc2d31db
domnitskiy.oleg <o.domnitskiy@appomart.com>
2024-12-03 20:17:16 +0400

    Update Circle component with new circle style and props

--- src/components/Circle/index.tsx
+++ src/components/Circle/index.tsx
@@ -1,24 +1,23 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { gsap } from 'gsap'
-import './styles.scss'
-import { relative } from 'path'
+import classes from './circle.module.scss'
 
 // start angle from 12 o'clock position
 const START_ANGLE = 30
 const LARGE_CIRCLE_RADIUS = 265
-const SMALL_CIRCLE_RADIUS = 16
+const SMALL_CIRCLE_RADIUS = 28
 
-export default function Circle({
-  numberOfCircles,
-}: {
+type Props = {
   numberOfCircles: number
-}) {
+  currentPeriodIndex: number
+}
+export default function Circle(props: Props) {
+  const { numberOfCircles, currentPeriodIndex } = props
   const periodAngle = useMemo(() => {
     return 360 / numberOfCircles
   }, [numberOfCircles])
   const elementsRef = useRef<HTMLDivElement[]>([])
 
-  const smallCircleRef = useRef<HTMLDivElement>(null)
   const circleRef = useRef<HTMLDivElement>(null)
 
   const [lastRotationAngle, setLastRotationAngle] = useState(0)
@@ -28,28 +27,28 @@ export default function Circle({
     gsap.to(elementsRef.current[index], {
       width: 56,
       height: 56,
-      opacity: 0,
+      backgroundColor: '#F5F5F5',
       duration: 0.5,
       ease: 'none',
     })
-    console.log(123)
   }, [])
 
   const onMouseLeave = useCallback((index: number) => {
     gsap.to(elementsRef.current[index], {
       width: 6,
       height: 6,
-      opacity: 1,
+      backgroundColor: '#42567A',
       duration: 0.5,
       ease: 'none',
     })
-    console.log(123)
   }, [])
 
   const onClick = useCallback(
     (newActiveIndex: number) => {
       if (!circleRef.current) return
       const rotateAngle = (activeIndex - newActiveIndex) * periodAngle
+
+      onMouseLeave(activeIndex)
       setAcitveIndex(newActiveIndex)
       setLastRotationAngle(rotateAngle + lastRotationAngle)
 
@@ -57,7 +56,7 @@ export default function Circle({
         rotateAngle + lastRotationAngle
       }deg)`
     },
-    [activeIndex, periodAngle, lastRotationAngle],
+    [activeIndex, periodAngle, lastRotationAngle, onMouseLeave],
   )
 
   const circles = Array.from({ length: numberOfCircles }, (_, i) => {
@@ -73,54 +72,42 @@ export default function Circle({
     return { x, y }
   })
 
+  useEffect(() => {
+    console.log(currentPeriodIndex)
+    onMouseEnter(currentPeriodIndex)
+    onClick(currentPeriodIndex)
+  }, [currentPeriodIndex])
+
   return (
-    <div className='container'>
-      <div ref={circleRef} className='outer-circle'>
+    <div className={classes.container}>
+      <div ref={circleRef} className={classes.outerCircle}>
         {circles.map((circle, index) => (
           <div
+            onMouseEnter={() => onMouseEnter(index)}
+            onMouseLeave={() => {
+              if (index === activeIndex) return
+              onMouseLeave(index)
+            }}
             onClick={() => {
               if (index === activeIndex) return
               onClick(index)
             }}
             key={index}
-            className='small-circle'
+            className={classes.smallCircleBase}
             style={{
               left: circle.x,
               top: circle.y,
               transform: `rotate(${-lastRotationAngle}deg)`,
-              backgroundColor: 'gainsboro',
-              //   overflow: 'hidden',
             }}
           >
             <div
-              onMouseEnter={() => onMouseEnter(index)}
-              onMouseLeave={() => onMouseLeave(index)}
               ref={(el) => {
                 if (!el) return
                 elementsRef.current[index] = el
               }}
-              style={{
-                position: 'absolute',
-                width: 6,
-                height: 6,
-                top: '50%',
-                left: '50%',
-                borderRadius: '50%',
-                transform: ' translate(-50%, -50%)',
-                backgroundColor: '#42567A',
-                overflow: 'hidden',
-              }}
+              className={classes.smallCircleVisible}
             >
-              <p
-                style={{
-                  position: 'relative',
-                  margin: 'auto',
-                  top: '50%',
-                  transform: 'translateY(-50%)',
-                }}
-              >
-                {index}
-              </p>
+              <p className={classes.period}>{index + 1}</p>
             </div>
           </div>
         ))}
